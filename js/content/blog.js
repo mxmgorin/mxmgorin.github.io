b6df9628fa879ea948f063f9553607bc58921a08
Troidem <onlinexmax@gmail.com>
2025-12-31 16:03:12 +0300

    feat: improve wording

--- js/content/blog.js
+++ js/content/blog.js
@@ -1,15 +1,15 @@
 export const blogView = [
   {
-    name: "Testing CHIP-8 VM",
+    name: "Testing My CHIP-8 VM: Deterministic Execution and Golden PNG Validation",
     desc: {
       en: "Deterministic testing of a CHIP-8 virtual machine using ROM-based execution and golden file comparison.",
-      ru: "Детерминированное тестирование виртуальной машины CHIP-8 через запуск ROM-файлов с последующим сравнением вывода программы с эталонными результатами (golden-файлами), представленными в виде PNG-изображений.",
+      ru: "Детерминированное тестирование виртуальной машины CHIP-8 используя ROM-файлы с последующим сравнением вывода программы с эталонными результатами (golden-файлами), представленными в виде PNG-изображений.",
     },
     url: "https://github.com/mxmgorin/ch8go/wiki/Testing",
     tags: ["Go", "Emulation", "CHIP-8"],
   },
   {
-    name: "Designing a CHIP-8 VM Architecture",
+    name: "Designing My CHIP-8 VM: Architecture Overview",
     desc: {
       en: "Exploring the key design decisions behind my CHIP-8 virtual machine architecture — component structure, execution flow, code organization, and implementation strategy.",
       ru: "Разбор того, как устроена моя виртуальная машина CHIP-8: из каких компонентов она состоит, как выполняет инструкции, как организован код и почему я выбрал именно такую архитектуру.",
@@ -18,9 +18,9 @@ export const blogView = [
     tags: ["Go", "Emulation", "CHIP-8"],
   },
   {
-    name: "CHIP‐8 System Overview",
+    name: "Exploring CHIP-8: Getting to know the system",
     desc: {
-      en: "A overview of the CHIP-8 virtual machine, its historical variants, and the core components that define it — memory model, registers, timers, display system, and the instruction execution cycle.",
+      en: "An overview of the CHIP-8 virtual machine, its historical variants, and the core components that define it — memory model, registers, timers, display system, and the instruction execution cycle.",
       ru: "Обзор виртуальной машины CHIP-8, её исторических вариантов и основных компонентов: памяти, процессора, регистров, таймеров, дисплея.",
     },
     url: "https://github.com/mxmgorin/ch8go/wiki/CHIP%E2%80%908-System",
