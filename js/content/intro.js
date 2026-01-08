443fdd9b97ea73996f52fc2e1264ae762b8183d0
Troidem <onlinexmax@gmail.com>
2026-01-08 16:03:37 +0300

    feat: improve footer, intro

--- js/content/intro.js
+++ js/content/intro.js
@@ -4,16 +4,12 @@ export const introView = {
 Here you can read about my projects, professional work, and blog posts.
 
 An optional command input is available at the bottom of the screen.
-You can browse the content using commands or by navigating with the keyboard or mouse.
-
-Type 'help' to get started.`,
+You can browse the content using commands or by navigating with the keyboard or mouse.`,
 
   ru: `Добро пожаловать! Это мой личный сайт.
 
 Здесь вы можете узнать о моих проектах, профессиональном опыте и прочитать блог.
 
 Внизу экрана доступна командная строка.
-Вы можете управлять сайтом с помощью команд или использовать клавиатуру и мышь.
-
-Введите 'help', чтобы начать.`,
+Вы можете управлять сайтом с помощью команд или использовать клавиатуру и мышь.`,
 };
